cd029bc11aaab6765e2cf6395b1caf4006579c52
Meng Mu <1436934505qq.com>
2019-11-12 00:09:13 +0800

    11月12日提交

---
+++ learn_1/learn_1/test_1.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//int Max(int x, int y) {
+//	if (x > y) {
+//		return (x);
+//	}
+//	else
+//	{
+//		return (y);
+//	}
+//
+//}
+//
+//int main (){
+//	int a = 10;
+//	int b = 20;
+//	int ret = Max(a, b);
+//	printf("%d", ret);
+//
+//	system("pause");
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
